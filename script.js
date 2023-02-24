684d222012a9bf9a30af3b071cb2bd1d627f6430
Tsegay Hailemariam <tsegayhailemariam444@gmail.com>
2023-02-24 19:50:45 +0300

    Javascript DOM Manipulation

---
+++ script.js
@@ -0,0 +1,47 @@
+"strict";
+/*
+//Javascript getElementById() method
+const p = document.getElementById("message");
+console.log(p);
+
+
+//Javascript getElementsByName() method
+let btnSubmit = document.getElementById("btnRate");
+let outPut = document.getElementById("output");
+btnSubmit.addEventListener("click", () => {
+  let rates = document.getElementsByName("rate");
+  for (let i = 0; i < rates.length; i++) {
+    if (rates[i].checked) {
+      outPut.textContent = `Congratulations you selected ${rates[i].value}`;
+    }
+  }
+});
+
+//Javascript getElementsByTagName() method
+let btnCount = document.getElementById("btnCount");
+btnCount.addEventListener("click", () => {
+  let headings = document.getElementsByTagName("h2");
+  let heading2 = document.getElementById("heading2");
+  for (let i = 0; i < headings.length; i++) {
+    console.log(headings[i].textContent);
+  }
+});
+
+//JavaScript getElementsByClassName
+let menu = document.getElementById("menu");
+let items = menu.getElementsByClassName("item");
+
+let data = [].map.call(items, (item) => item.textContent);
+
+console.log(data);
+
+//Javascript map
+let map = new Map();
+
+map.set("1", "str1");
+map.set(1, "num1");
+map.set(true, "bool1");
+console.log(map.get(1));
+console.log(map.has("1"));
+console.log(map.size);
+*/
